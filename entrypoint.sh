54290228fe487fa316276c4302d4406d1c48f5ee
Alex Boten <alex@boten.ca>
2023-08-19 23:12:12 -0700

    add output-path input
    Signed-off-by: Alex Boten <alex@boten.ca>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 export GOARCH=$TARGETARCH
 export CGO_ENABLED=0
 export GOFLAGS=-buildvcs=false
+mkdir -p ./$2
 echo "Building using /builder --config $1"
-/builder --config $1
+/builder --config $1 --output-path ./$2/otelcolaction
 
 # time=$(date)
 # echo "time=$time" >> $GITHUB_OUTPUT
