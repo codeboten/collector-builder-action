dc0242b203fcc612a9066497678b67bb264e5132
Alex Boten <alex@boten.ca>
2023-08-19 23:13:42 -0700

    update entrypoint script to make output readable
    Signed-off-by: Alex Boten <alex@boten.ca>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ export GOARCH=$TARGETARCH
 export CGO_ENABLED=0
 export GOFLAGS=-buildvcs=false
 mkdir -p ./$2
+chmod a+r ./$2
 echo "Building using /builder --config $1"
 /builder --config $1 --output-path ./$2/otelcolaction
 
